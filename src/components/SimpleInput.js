2a69fee4edb074caee3b35617d4db695be03451c
Glen Adamson <glen.adamson@terraquest.co.uk>
2021-08-09 09:00:48 +0100

    onchange and onsubmit

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,9 +1,33 @@
+import {useRef, useState} from "react";
+
 const SimpleInput = (props) => {
+    const [enteredName, setEnteredName] = useState('');
+    const [enteredNameIsValid, setEnteredNameIsValid] = useState(true);
+    const nameRef = useRef()
+
+    const nameInputChangeHandler = (event) => {
+        setEnteredName(event.target.value);
+    }
+
+    const formSubmissiveHandler = (event) => {
+        event.preventDefault();
+
+        if (enteredName.trim() === '') {
+            setEnteredNameIsValid(false)
+            return;
+        }
+
+        setEnteredNameIsValid(true)
+        console.log(nameRef.current.value);
+        setEnteredName('');
+    }
+
   return (
-    <form>
+    <form onSubmit={formSubmissiveHandler}>
       <div className='form-control'>
         <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+        <input ref={nameRef} type='text' id='name' onChange={nameInputChangeHandler} value={enteredName} />
+          {!enteredNameIsValid && <p>name must be valid</p>}
       </div>
       <div className="form-actions">
         <button>Submit</button>
